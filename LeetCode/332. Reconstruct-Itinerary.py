9b36d793dcdc398ae870f96a70cedf976f98eec1
sseunghyuns <allindata96@gmail.com>
2022-03-04 12:01:18 +0900

    Update leetcode-study with ai-boostcamp cv4 team

---
+++ LeetCode/332. Reconstruct-Itinerary.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+'''
+https://leetcode.com/problems/reconstruct-itinerary/
+'''
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+
+        graph = defaultdict(list)
+        for a, b in sorted(tickets):
+            graph[a].append(b)
+
+        result = []
+        print(graph)
+        def dfs(a):
+            while graph[a]:
+                print(a)
+                dfs(graph[a].pop(0))
+            result.append(a)   
+        
+        dfs('JFK')
+        print(result[::-1])
+        return result[::-1]
+
+    
+    
+    
+    
