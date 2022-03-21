248b3010b0511460de1911c13fa9af3d33d62ac8
sseunghyuns <allindata96@gmail.com>
2022-03-21 10:27:25 +0900

    BOJ greedy 잃어버린 괄호

--- BoJ/1541번_잃어버린_괄호.py
+++ BoJ/1541번_잃어버린_괄호.py
@@ -1,34 +1,38 @@
+'''
+양수 +, -
+1. 첫 "-" 나올 때 괄호 열기 (최대한 많은 수 빼기?)
+2. 두 번째 "-"가 나오면 열린 괄호 먼저 닫고, 다시 열기
+'''
+
 eq = input()
 
-data = []
 temp = ''
+answer = ''
 is_open = False
 
 for i in eq:
     if i not in ['+', '-']:
-        temp+=i
-        
-    elif i == '-':
-        if is_open: # 괄호가 열린 적이 있으면
-            data.append(str(int(temp)))
-            data.append(')') # 괄호 닫기
-            data.append(i)
-            data.append('(') # 괄호 열기    
-            temp= ''
-        else: # 괄호가 열린 적이 없으면
-            data.append(str(int(temp)))
-            data.append(i)
-            data.append('(') # 괄호 열기    
-            temp = ''
-            is_open = True
-    else:
-        data.append(str(int(temp)))
-        data.append(i)
-        temp = ''
-data.append(str(int(temp))) # 마지막 숫자
-if is_open:
-    data.append(')')
-print(data)
+        temp += i 
 
-answer = "".join(data)
+    elif i=='-': 
+        if is_open: # 두 번째 "-"
+            answer += str(int(temp))
+            answer += ')'+i+'('
+            temp = ''
+            
+        else: # 첫 "-"
+            answer +=str(int(temp))
+            answer += i
+            answer += '('
+            temp = ''
+            is_open=True
+    else:
+        answer += str(int(temp))
+        answer += i
+        temp = ''
+
+answer += str(int(temp)) # 마지막 숫자
+
+if is_open:
+    answer +=')'
 print(eval(answer))
