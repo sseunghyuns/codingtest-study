78ae5650e1077b3dfa1d7540745f967dd77e4d6e
sseunghyuns <allindata96@gmail.com>
2022-07-23 23:40:39 +0900

    [이코테] 팀 결성

--- 이코테/Graph-Theory/팀_결성.py
+++ 이코테/Graph-Theory/팀_결성.py
@@ -1,26 +1,26 @@
 def find(parent, x):
-    if parent[x]!=x:
+    if parent[x] != x:
         parent[x] = find(parent, parent[x])
-    return x # x
-    
+    return parent[x]
+
 def union(parent, a, b):
     a = find(parent, a)
     b = find(parent, b)
-    
     if a<b:
-        parent[b]=a
+        parent[b] = a
     else:
-        parent[a]=b
-        
-N, M = map(int, input().split())
-parent = [i for i in range(N+1)]    
-    
+        parent[a] = b
+
+N, M =map(int, input().split())
+parent = [i for i in range(N+1)]
+
 for _ in range(M):
-    op = list(map(int, input().split()))
-    if op[0]==0:
-        union(parent, op[1], op[2])
+    o, a, b = input().split()
+    a, b = int(a), int(b)
+    if o == '0':
+        union(parent, a, b)
     else:
-        if find(parent, op[1])==find(parent, op[2]):
-            print("YES")
+        if find(parent, a) == find(parent, b):
+            print('YES')
         else:
             print("NO")
