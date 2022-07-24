b20a1ebefdc8a12e6719db10b1a2b190f0f92a9b
sseunghyuns <allindata96@gmail.com>
2022-07-24 12:41:27 +0900

    [이코테] 도시 분할 계획

--- 이코테/Graph-Theory/도시_분할_계획.py
+++ 이코테/Graph-Theory/도시_분할_계획.py
@@ -1,37 +1,39 @@
 import sys
 input = sys.stdin.readline
 
-def find_parent(parent, x):
-    if parent[x]!=x:
-        parent[x] = find_parent(parent, parent[x])
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
     return parent[x]
-
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
+    
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
     if a<b:
-        parent[b]=a
+        parent[b] = a 
     else:
-        parent[a]=b
+        parent[a] = b
 
 N, M = map(int, input().split())
 parent = [i for i in range(N+1)]
-edges = []
+edges = [] # 최소 신장 트리 
 
 for _ in range(M):
-    a, b, c = map(int, input().split())
-    edges.append((c, a, b))
+    A, B, C = map(int, input().split())
+    edges.append((C, A, B))
+
 edges.sort()
 
-result = 0 # 전체 비용
-max_cost = 0 # edge 중 최대 비용
-
-for edge in edges:
-    c, a, b = edge
-    if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, a, b)
-        result+=c
-        if max_cost<c:
-            max_cost=c
-            
-print(result-max_cost) # 마지막으로 추가된 edge 비용 제외
+def solution(parent, edges):
+    result = 0 # 최종 유지비
+    max_cost = 0
+    for e in edges:
+        cost, a, b = e
+        if find(parent, a) != find(parent, b):
+            union(parent, a, b)
+            result += cost
+            if max_cost < cost:
+                max_cost = cost
+    print(result-max_cost)
+    
+solution(parent, edges)
