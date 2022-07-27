a30d03da3304e251d865e64d62248d2bc06c9020
sseunghyuns <allindata96@gmail.com>
2022-07-27 23:01:50 +0900

    [이코테] 커리큘럼

--- 이코테/Graph-Theory/커리큘럼.py
+++ 이코테/Graph-Theory/커리큘럼.py
@@ -1,37 +1,39 @@
 from collections import deque
-import copy
+from copy import deepcopy
 
 N = int(input())
+
+indegree = [0]*(N+1)
 graph = [[] for _ in range(N+1)]
 time = [0] * (N+1)
-indegree = [0] * (N+1)
 
 for i in range(1, N+1):
     data = list(map(int, input().split()))
     time[i] = data[0]
-    for x in data[1:-1]:
-        graph[x].append(i) # graph[1].append(2) 2번을 듣기 위해선 1번이 우선 수강되어야 함.
-        indegree[i] += 1
-        
+    for j in data[1:-1]:
+        indegree[i]+=1
+        graph[j].append(i) # graph[1].append(2) 2번을 듣기 위해선 1번이 우선 수강되어야 함.
+
+print(indegree)
+print(graph)
+
 def topology_sort():
-    result = copy.deepcopy(time)
+    result = deepcopy(time)
     q = deque()
     
     for i in range(1, N+1):
         if indegree[i]==0:
             q.append(i)
-    
+            
     while q:
         now = q.popleft()
-        
         for i in graph[now]:
+            result[i] = max(result[now] + time[i], result[i])
             indegree[i]-=1
+            
             if indegree[i]==0:
                 q.append(i)
                 
-            result[i] = max(time[i]+result[now], result[i]) # 최소 시간 업데이트
-            
-    for r in result[1:]:
-        print(r)
-
-topology_sort()    
+    for i in range(1, N+1):
+        print(result[i])
+topology_sort()
